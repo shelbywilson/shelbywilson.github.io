871157645d16da26e9e749ef9419a0c3c71f68a9
shelby <s.wilson024@gmail.com>
2017-12-22 12:14:44 -0600

    removing unused files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./'));
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
--- src/app/components/news/NewsfeedFocusedArticle.jsx
+++ src/app/components/news/NewsfeedFocusedArticle.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class NewsfeedFocusedArticle extends React.Component {
 	constructor(props) {
@@ -15,18 +16,32 @@ class NewsfeedFocusedArticle extends React.Component {
 		e.stopPropagation(e)
 	}
 	render() {
-		console.log(this.props.article)
+		const article = this.props.article;
 		if (this.props.article) {
 			return (
 				<div className='newsfeed-focused-article' onMouseUp={this.toggleFocusedArticle}>
 					<div className='newsfeed-focused-article-content' onMouseUp={this._stopPropagation} >
 						<h2>
-							{this.props.article.title}
+							{article.title}
 						</h2>
 						<h3>
 							{this.props.source.name}
 						</h3>
-						<img src={this.props.article.urlToImage} alt='' />
+						<h3>
+							{moment(article.publishedAt).fromNow()}
+						</h3>
+						<div className="newsfeed-focused-article-description">
+							{article.description ?
+								<p>
+									{article.urlToImage &&
+										<img src={article.urlToImage} alt='' />
+									}
+									{article.description}
+								</p>
+								:
+								<img src={article.urlToImage} className='full-size' alt='' />
+							}
+						</div>
 					</div>
 				</div>
 			)
