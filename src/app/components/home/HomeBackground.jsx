2f43c44dfaeb811d63102e395cd68afd441704fc
Shelby Wilson <s.wilson024@gmail.com>
2018-01-10 17:21:47 -0600

    update homepage animation

--- src/app/components/home/HomeBackground.jsx
+++ src/app/components/home/HomeBackground.jsx
@@ -21,7 +21,7 @@ class HomepageBackground extends React.Component {
 		setTimeout(function () {
 			this.updateSvg();
 			$('body').addClass('home-loaded');
-		}.bind(this), 1000);
+		}.bind(this), 2000);
 	}
 	componentWillUnMount() {
 		window.removeEventListener("resize");
@@ -29,123 +29,92 @@ class HomepageBackground extends React.Component {
 	setSvg() {
 	    let svg = d3.select(this.refs.svg);
 	    let group;
-	    let i;
 
 	    svg.attr('width',window.innerWidth);
 	    svg.attr('height',window.innerHeight);
 
 	    const data = this._getInitialData(window.innerWidth, window.innerHeight);
 
-	    for (i = 0; i < data.length; i += 1) {
-	    	group = svg.append('g')
-	    		.attr('class', 'group-' + i);
+    	group = svg.append('g')
+    		.attr('class', 'group');
 
-	    	group.attr('transform', 'translate(' + window.innerWidth*0.6 + ',' + window.innerHeight*0.4 +')');
+    	group.attr('transform', 'translate(' + window.innerWidth*0.6 + ',' + window.innerHeight*0.4 +')');
 
-		    group.selectAll('.line')
-		    	.data(data[i])
-		    	.enter()
-				.append('path')
-				.attr('class', 'line')
-              	.attr('d', function(d) { return lineFunction(d); })
-              	.attr('transform', 'rotate(0)');
-	    }
+	    group.selectAll('.line')
+	    	.data(data)
+	    	.enter()
+			.append('path')
+			.attr('class', 'line')
+          	.attr('d', function(d) { return lineFunction(d); })
+          	.attr('transform', 'rotate(0)');
 	}
     updateSvg() {
 	    let svg = d3.select(this.refs.svg);
 	    let group;
-	    let i;
-
-	    console.log('update')
 
 	    svg.attr('width',window.innerWidth);
 	    svg.attr('height',window.innerHeight);
 
 	    const data = this._getUpdatedData(window.innerWidth, window.innerHeight);
+    	group = svg.select('.group');
 
-	    for (i = 0; i < data.length; i += 1) {
-	    	group = svg.select('.group-' + i);
-
-		    group.selectAll('.line')
-		    	.data(data[i])
-		    	.transition()
-		    	.attr('transform', function(d,i) { return 'rotate(' + i + ')'; })
-		    	.duration(1200)
-	            .attr('d', function(d) { return lineFunction(d); });
-	    }
+	    group.selectAll('.line')
+	    	.data(data)
+	    	.transition()
+	    	.attr('transform', function(d,i) { return 'rotate(' + i + ')'; })
+	    	.duration(1200)
+            .attr('d', function(d) { return lineFunction(d); });
     }
     resizeSvg() {
     	let svg = d3.select(this.refs.svg);
-	    let group;
-	    let i;
 
 	    svg.attr('width',window.innerWidth);
 	    svg.attr('height',window.innerHeight);
 
-	    for (i = 0; i < data.length; i += 1) {
-	    	group = svg.select('.group-' + i);
-
-	    	group.attr('transform', 'translate(' + window.innerWidth*0.6 + ',' + window.innerHeight*0.4 +')');
-	    }
+    	svg.select('.group')
+    		.attr('transform', 'translate(' + window.innerWidth*0.6 + ',' + window.innerHeight*0.4 +')');
     }
     _getInitialData(width, height) {
-    	let i;
-    	let j;
-    	let data = [];
-    	let temp;    	
+    	let i = 1;
+    	let data = [];	
     	const h = height /30;
 
-    	//for (i = 0; i < 3; i += 1) {
-    		j = 1;
-    		temp = [];
-	    	do {
-
-	    		temp.push([
-		    		{
-		    			x: -width * Math.random(),
-		    			y: ((j/90 + 100/j) * h)
-		    		},
-		    		{
-		    			x: width * Math.random(),
-		    			y: ((j/90 + 100/j) * h)
-		    		}
-		    	]);
-	    		j += 1;
-	    	} while (j < 180);
-
-	    	data.push(temp);
-	    //}
+    	do {
+    		data.push([
+	    		{
+	    			x: -width * Math.random(),
+	    			y: ((i/90 + 100/i) * h)
+	    		},
+	    		{
+	    			x: width * Math.random(),
+	    			y: ((i/90 + 100/i) * h)
+	    		}
+	    	]);
+    		i += 1;
+    	} while (i < 180);
 
     	return data;
     }
 
     _getUpdatedData(width, height) {
-    	let i;
-    	let j;
+    	let i = 1;
     	let data = [];
-    	let temp;
     	const h = height /30;
+    	const max = Math.max(width, height);
 
-    	//for (i = 0; i < 3; i += 1) {
-    		j = 1;
-    		temp = [];
-	    	do {
-
-	    		temp.push([
-		    		{
-		    			x: -width * 0.4,
-		    			y: ((j/90 + 100/j) * h)
-		    		},
-		    		{
-		    			x: width * 0.8,
-		    			y: ((j/90 + 100/j) * h)
-		    		}
-		    	]);
-	    		j += 1;
-	    	} while (j < 180);
-
-	    	data.push(temp);
-	    //}
+    	do {
+    		data.push([
+	    		{
+	    			x: -max * (12/i),
+	    			y: ((i/90 + 100/i) * h)
+	    		},
+	    		{
+	    			x: max * 0.8,
+	    			y: ((i/90 + 100/i) * h)
+	    		}
+	    	]);
+    		i += 1;
+    	} while (i < 180);
 
     	return data;
     }
