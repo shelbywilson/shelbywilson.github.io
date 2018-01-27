815593442a2927858482a56b057935df68efb18e
Shelby Wilson <s.wilson024@gmail.com>
2018-01-26 20:20:11 -0600

    analysis by news source

--- src/app/components/news/NewsfeedAbout.jsx
+++ src/app/components/news/NewsfeedAbout.jsx
@@ -50,7 +50,7 @@ class NewsfeedAbout extends React.Component {
 	        	.append('g');
 
 	        group.append('circle')
-	        	.style('fill', function(d,i) {return d.children ? 'transparent' : d.data.color;})
+	        	.style('fill', function(d,i) {return d.data.color || 'none';})
 	        	.attr('cx', function (d) { return d.x; })
 	            .attr('cy', function (d) { return d.y; })
 	            .attr('r', function (d) { return d.r; });
@@ -59,9 +59,9 @@ class NewsfeedAbout extends React.Component {
 	        	.attr('class', 'label')
 	        	.attr('x', function (d) { return d.x; })
 	            .attr('y', function (d) { return d.y; })
-	            .append('tspan') 
 	        	.text(function(d) { return d.children ? '' : d.data.name; })
-	        	.style('text-anchor', 'middle');
+	        	.style('text-anchor', 'middle')	        	
+	        	.call(wrap);
 	    }
 	}
 	triggerUpdate() {
@@ -70,19 +70,46 @@ class NewsfeedAbout extends React.Component {
 		})
 	}
 	getData() {
-		let data = [];
-		let i;
+		let data;
+		let temp;
+		let children = [];
 
-		for (i in this.props.countBySource) {
-			data.push({
-				category: this.props.sourcesDictionary[i].category,
-				name: this.props.sourcesDictionary[i].name,
-				value: this.props.countBySource[i],
-				color: colors[this.props.sourcesDictionary[i].category]
-			});
+		let i;
+		let j;
+		for (i in this.props.count) {
+			temp = [];
+			for (j in this.props.countBySource) {
+				if (this.props.sourcesDictionary[j].category === i) {
+					temp.push({
+						category: this.props.sourcesDictionary[j].category,
+						name: this.props.sourcesDictionary[j].name,
+						value: this.props.countBySource[j],
+						color: 'rgba(255,255,255,0.25)'
+					});
+				}
+			}
+			children.push({
+				name: i,
+				color: colors[i],
+				children: temp
+			})
 		}
 
-		return {children: data};
+
+		// let temp = [];
+		// let data = {};
+		// let i;
+
+		// for (i in this.props.countBySource) {
+		// 	data.push({
+		// 		category: this.props.sourcesDictionary[i].category,
+		// 		name: this.props.sourcesDictionary[i].name,
+		// 		value: this.props.countBySource[i],
+		// 		color: colors[this.props.sourcesDictionary[i].category]
+		// 	});
+		// }
+
+		return {children: children};
 	}
 	render() {
 		return (
