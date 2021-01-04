19f216b4e1ef008f5e8ec111d9bac0ca8d907bce
Shelby Wilson <s.wilson024@gmail.com>
2021-01-03 22:15:47 -0600

    updating text and images

--- src/pages/home/content/Muriel.js
+++ src/pages/home/content/Muriel.js
@@ -3,6 +3,7 @@ import React from 'react';
 import muriel3 from './../../../images/home/muriel-3.png';
 import muriel from './../../../images/muriel/muriel.jpg';
 import muriel2 from './../../../images/muriel/muriel-2.jpg';
+import murielGif from './../../../images/muriel/muriel.gif';
 import murielrotating from './../../../images/muriel/muriel-rotating.png';
 import murielrotating2 from './../../../images/muriel/muriel-rotating-2.gif';
 
@@ -11,10 +12,10 @@ export default () => {
         <div>
             <img src={muriel3} alt='' />
             <p>
-                Some work from <a href='https://sfpc.io/' target='_blank'>SFPC</a> Fall 2019, while studying <a href='https://en.wikipedia.org/wiki/Muriel_Cooper' target='_blank'>Muriel Cooper</a> and her work with typography.
+                A kind of digital tapestry, the result of an experiment while studying <a href='https://en.wikipedia.org/wiki/Muriel_Cooper' target='_blank'>Muriel Cooper</a> and her work with typography at <a href='https://sfpc.io/' target='_blank'>SFPC</a> Fall 2019.
             </p>
             <p>
-                The vectors in each character are represented by vertical lines that are distributed evenly from left to right. Simpler characters (e.g. ‘l’ or ‘-’) have fewer points, more complex or round characters (e.g. ‘o’ or ’8’) have more. Color is determined by ASCII code, either red, yellow, or blue.
+                This application accepts text input. The vectors in each character are then represented by vertical lines that are distributed evenly from left to right. Simpler characters (e.g. ‘l’ or ‘-’) have fewer points, more complex or round characters (e.g. ‘o’ or ’8’) have more. Color is determined by ASCII code, either red, yellow, or blue.
             </p>
             <p>
                 Using: OpenFrameworks
@@ -23,22 +24,43 @@ export default () => {
                 View <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel' target='_blank'>Source code</a> and <a href='https://github.com/shelbywilson/sfpc-rtp' target='_blank'>more examples</a>.
             </p>
             <div className='img-container first'>
-                <img src={muriel2} alt='' />
+                <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel' 
+                    target='_blank'>
+                    <img src={muriel2} alt='' />
+                </a>
             </div>
             <div className='img-container'>
-                <img src={muriel} alt='' />
+                <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel' 
+                    target='_blank'>
+                    <img src={muriel} alt='' />
+                </a>
             </div>
             <div className='img-container'>
-                <img src={murielrotating} alt='' />
+                <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel' 
+                    target='_blank'
+                    className='img-container'>
+                    <img src={murielGif} alt='' />
+                </a>
+            </div>
+            <div className='img-container'>
+                <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel3' 
+                    target='_blank'
+                    className='img-container'>
+                    <img src={murielrotating} alt='' />
+                </a>
             </div>
             <p>
-                Characters rotate to appear as if looking at them from the side.
+                Another experiment in which characters appear to rotate as if looking at them from the side.
             </p>
             <p>
                 View <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel3' target='_blank'>Source code</a>.
             </p>
             <div className='img-container first' style={{height: 200, overflow: 'hidden'}}>
-                <img src={murielrotating2} alt='' />
+                <a href='https://github.com/shelbywilson/sfpc-rtp/tree/master/muriel3' 
+                    target='_blank'
+                    className='img-container'>
+                    <img src={murielrotating2} alt='' />
+                </a>
             </div>
         </div>
     )
