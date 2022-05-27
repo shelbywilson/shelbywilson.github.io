0f84508f5099c0dd9dc2b4fe8581b8f6fc27eb09
Shelby Wilson <s.wilson024@gmail.com>
2022-05-27 09:56:26 -0700

    new build

--- config/paths.js
+++ config/paths.js
@@ -107,5 +107,9 @@ module.exports = {
       output: 'tunnel/index.html',
       title: 'tunnel',
     },
+    daisies: {
+      output: 'daisies/index.html',
+      title: 'daisies',
+    },
   }
 }
