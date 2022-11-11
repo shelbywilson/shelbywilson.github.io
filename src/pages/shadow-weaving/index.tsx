c991f188cb19cbe158f73b1631b798a0ba043532
Shelby Wilson <s.wilson024@gmail.com>
2022-11-10 19:35:58 -0800

    updates shadow weaving

--- src/pages/shadow-weaving/index.tsx
+++ src/pages/shadow-weaving/index.tsx
@@ -1,9 +1,6 @@
 import React, { FunctionComponent, useState, CSSProperties, useEffect } from 'react';
 
-const colors = [
-    `rgba(0, 0, 255, 1)`,
-    `rgba(255, 255, 255, 1)`,
-]
+import './_shadow-weaving.scss';
 
 type Square = {
     c: number,
@@ -11,14 +8,17 @@ type Square = {
 }
 
 export const ShadowWeaving: FunctionComponent = () => {
-    const [warp, setWarp] = useState([1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0])
-    const [weft, setWeft] = useState([1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0])
-    const [hasGap, setHasGap] = useState(true);
+    const [warp, setWarp] = useState([0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0])
+    const [weft, setWeft] = useState([0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0])
+    const [gapSize, setGapSize] = useState(15);
+    const [showGrid, setShowGrid] = useState(true);
+    const [blackAndWhite, setBlackAndWhite] = useState(false);
 
     const size = 30;
-    const gap = hasGap ? 4 : 0;
+    const gap = 30 - gapSize;
+    const hasGap = gap > 1;
 
-    const getFabric = ():Array<Array<Square>> => {
+    const getFabric = (warp: Array<number> , weft: Array<number>):Array<Array<Square>> => {
         const arr = [];
         let i = 0;
 
@@ -63,29 +63,109 @@ export const ShadowWeaving: FunctionComponent = () => {
         }
     }
 
+    const checkImpossible = () => {
+        setFabric(prev => {
+            const fabric = [...prev];
+            fabric.forEach((row: Array<Square>, j: number) => {
+                row.forEach((square: Square, i: number) => {
+                    if (weft[j] !== warp[i]) {
+                        if (square.c === warp[i]) {
+                            square.v = 1;
+                        } else if (square.c === weft[j]) {
+                            square.v = 0;
+                        }
+                    } else if (square.c !== warp[i]) {
+                        // warp and weft are the same
+                        square.c = warp[i]
+                        square.v = Math.random() > 0.5 ? 1 : 0;
+                    }
+                })
+            })
+
+            return fabric
+        })
+    }
+
+    const randomize = () => {
+        const p = warp.map(() => Math.random() > 0.5 ? 1 : 0)
+        const f = weft.map(() => Math.random() > 0.5 ? 1 : 0)
+        
+        setWarp(p)
+        setWeft(f)
+        setFabric(getFabric(p, f))
+    }
+
+    const everyNth = (n: number) => {
+        const p = warp.map((x,i) => i%n === 0 ? 0 : 1)
+        const f = weft.map((y,j) => j%n === 0 ? 0 : 1)
+        
+        setWarp(p)
+        setWeft(f)
+        setFabric(getFabric(p, f))
+    }
+
     useEffect(() => {
-        setFabric(getFabric())
+        checkImpossible()
     }, [warp, weft])
 
-    const [fabric, setFabric] = useState(getFabric());
+    const [fabric, setFabric] = useState(getFabric(warp, weft));
 
     return (
-        <div style={{maxWidth: 800, margin: '0 auto'}}>
+        <div className={`shadow-weaving ${blackAndWhite ? 'black-and-white' : ''} ${hasGap ? 'show-warp-weft' : ''} ${showGrid ? 'show-grid' : ''}`}>
             <div style={{margin: '0 0 2rem 0'}}>
-                <label>
-                    <input type='checkbox' checked={hasGap} onChange={() => setHasGap(prev => !prev)} />
-                    show warp and weft
-                </label>
                 <div>
-                    <button style={{margin: '1rem 1rem 0 0'}} onClick={() => setFabric(getFabric())}>random</button>
-                    <button style={{margin: '1rem 1rem 0 0'}} onClick={() => setWarp(prev => {
+                    <label>
+                        <input type='checkbox' checked={blackAndWhite} onChange={() => setBlackAndWhite(prev => !prev)} />
+                        black and white
+                    </label>
+                    <label>
+                        <input type='checkbox' checked={showGrid} onChange={() => setShowGrid(prev => !prev)} />
+                        show grid
+                    </label>
+                    <label>
+                        <input type='range' 
+                            value={gapSize} 
+                            min={2} 
+                            max={size} 
+                            onChange={(e) => setGapSize(parseInt(e.target.value, 10))} />
+                    </label>
+                </div>
+                <div>
+                    <button onClick={randomize}>random</button>
+                    <button onClick={() => everyNth(2)}>alternate</button>
+                    <button onClick={() => everyNth(3)}>every third</button>
+                </div>
+                <div>
+                    <button onClick={() => setWarp(prev => {
                         const updated = [...prev];
-                        updated.unshift(updated.length % 2)
+                        const c = updated.length % 2;
+                        updated.unshift(c)
+
+                        setFabric(prev => {
+                            const fabric = [...prev]
+                            fabric.forEach((row,i) => (
+                                row.unshift({c: c, v: c === weft[i] ? 0 : 1})
+                            ))
+                            return fabric
+                        })
+
                         return updated;
                     })}>+ warp</button>
                     <button style={{margin: '1rem 1rem 0 0'}} onClick={() => setWeft(prev => {
                         const updated = [...prev];
-                        updated.unshift(updated.length % 2)
+                        const c = updated.length % 2;
+                        updated.unshift(c)
+
+                        setFabric(prev => {
+                            const fabric = [...prev]
+                            const row = []
+                            while (row.length < warp.length) {
+                                row.push({c: c, v: c === warp[row.length] ? 1 : 0})
+                            }
+                            fabric.unshift(row)
+                            return fabric
+                        })
+
                         return updated;
                     })}>+ weft</button>
                 </div>
@@ -101,6 +181,26 @@ export const ShadowWeaving: FunctionComponent = () => {
                             onChange={() => setWeft(prev => {
                                 const updated = [...prev]
                                 updated[j] = f === 1 ? 0 : 1
+
+                                setFabric(prev => {
+                                    const fabric = [...prev]
+                                    let vCount = 1;
+                                    fabric[j].forEach((square, i) => {
+                                        if (square.v) {
+                                            vCount += 1;
+                                        }
+                                        square.c = updated[j]
+                                        if (square.c === warp[i]) {
+                                            // warp and weft are the same
+                                            square.v = vCount % 2;
+                                            vCount += 1;
+                                        } else {
+                                            square.v = 0;
+                                        }
+                                    })
+                                    return fabric
+                                })
+
                                 return updated
                             })} />
                     ))}
@@ -114,6 +214,27 @@ export const ShadowWeaving: FunctionComponent = () => {
                             onChange={() => setWarp(prev => {
                                 const updated = [...prev]
                                 updated[i] = p === 1 ? 0 : 1
+
+                                setFabric(prev => {
+                                    const fabric = [...prev]
+                                    fabric.forEach((row: Array<Square>, x) => {
+                                        row[i].c = updated[i] 
+                                        let vCount = 0;
+                                        if (updated[i] === weft[x]) {
+                                            // warp and weft are the same
+                                            row[i].v = vCount % 2;
+                                            vCount += 1;
+                                        } else {
+                                            // warp and weft are different
+                                            if (row[i].c === weft[x]) {
+                                                row[i].v = 0;
+                                            }
+                                        }
+
+                                    })
+                                    return fabric
+                                })
+
                                 return updated
                             })} />
                     ))}
@@ -125,26 +246,25 @@ export const ShadowWeaving: FunctionComponent = () => {
                                 width={size - gap}
                                 height={size * weft.length}
                                 x={i * size}
-                                style={{fill: colors[f], stroke: '#000'}}>
+                                className={`color-${f} grid-border warp`}>
                             </rect>
                         ))}
                         {fabric.map((row: Array<Square>, j) => (
                             <g key={j}>
                                 <rect key={`${j}-weft`} 
-                                    width={size * weft.length}
+                                    width={size * warp.length}
                                     height={size - gap}
                                     y={j * size}
-                                    style={{fill: colors[weft[j]], stroke: '#000'}}>
+                                    className={`color-${weft[j]} grid-border weft`}>
                                 </rect>
                                 {row.map((col: Square, i) => {
                                     const {c, v} = col;
-                                    const style: CSSProperties = {
-                                        fill: colors[c],
-                                    }
+                                    const style: CSSProperties = {}
                                     let width = size;
                                     let height = size;
                                     let x = i * size;
                                     let y = j * size;
+                                    let classes = `color-${c} ${weft[j] !== warp[i] ? 'changeable' : 'toggle-direction'}`
 
                                     if (hasGap) {
                                         width = size - gap + 2;
@@ -159,9 +279,8 @@ export const ShadowWeaving: FunctionComponent = () => {
                                             y -= 2;
 
                                             style.strokeDasharray = `${height} ${width}`;
-                                            style.strokeDashoffset = v ? height : 0;
-                                            style.stroke = '#000';
-                                            style.strokeWidth = 1;
+                                            style.strokeDashoffset = height;
+                                            classes += ' grid-border';
                                         }
                                     } else {
                                         width += 2;
@@ -174,6 +293,7 @@ export const ShadowWeaving: FunctionComponent = () => {
                                         <rect key={`${i}-${j}`} 
                                             height={height}
                                             width={width}
+                                            className={classes}
                                             x={x}
                                             y={y}
                                             onClick={() => toggle(i, j)}
